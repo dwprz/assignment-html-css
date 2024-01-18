6412fd9c7d0c9f864b65ecae44acf53e3686f954
Dwi Prasetiyo <dwiprasetiyo223@gmail.com>
2024-01-18 16:43:02 +0700

    feat: login

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#8053FF",
+        blue: "#007AFF",
+        "black-primary": "#333333",
+        "black-secondary": "#1A1A1A",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
